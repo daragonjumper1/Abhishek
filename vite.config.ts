6af506f0dc7c975db144b8537313af05d726b5a5
daragonjumper <chaturvedi760@gmail.com>
2025-02-27 22:08:25 +0530

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-project",
+  base: "/Abhishek/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Allows "@/components/ui/toaster" to work
+    },
+  },
 });
